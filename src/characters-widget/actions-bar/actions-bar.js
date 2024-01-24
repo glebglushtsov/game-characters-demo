5c66f887d415e3599265caf49bc9a08eb266f2fa
Gleb Glushtsov <gleb.glushtsov@gmail.com>
2024-01-24 13:52:28 -0800

    added uuid, added functionality to actions-bar

---
+++ src/characters-widget/actions-bar/actions-bar.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { useCharactersDispatch } from '../characters-context';
+import { images } from "../images";
+
+import styles from './actions-bar.module.css';
+
+function ActionsBar() {
+  const dispatch = useCharactersDispatch();
+  console.log('Controls');
+
+  function handleAddClick() {
+    dispatch({ type: 'create', payload: { id: uuidv4(), health: 0, attack: 0, defense: 0, image: images[Math.floor(Math.random() * 3)] } });
+  }
+
+  function handleAttackAllClick() {
+    dispatch({ type: 'attackAll' });
+  }
+
+  return (
+    <div className={styles.actionsBar}>
+      <button onClick={handleAddClick}>Add character</button>
+      <button onClick={handleAttackAllClick}>Attack all characters</button>
+    </div>
+  );
+}
+
+export default ActionsBar;
